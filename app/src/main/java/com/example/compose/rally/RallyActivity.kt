cd0546a742692bd94c3995e4ceffee45080b98dd
Sifanurfaizah <sifa.nur@student.umn.ac.id>
2025-03-03 14:47:04 +0700

    10. Testing Compose Navigation

--- app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -19,25 +19,14 @@ package com.example.compose.rally
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.compose.rally.Overview.rallyTabRowScreens
-import com.example.compose.rally.ui.accounts.AccountsScreen
-import com.example.compose.rally.ui.accounts.SingleAccountScreen
-import com.example.compose.rally.ui.bills.BillsScreen
 import com.example.compose.rally.ui.components.RallyTabRow
-import com.example.compose.rally.ui.overview.OverviewScreen
 import com.example.compose.rally.ui.theme.RallyTheme
 
 /**
@@ -57,61 +46,26 @@ class RallyActivity : ComponentActivity() {
 fun RallyApp() {
     RallyTheme {
         val navController = rememberNavController()
-
         val currentBackStack by navController.currentBackStackEntryAsState()
         val currentDestination = currentBackStack?.destination
+        val currentScreen =
+            rallyTabRowScreens.find { it.route == currentDestination?.route } ?: Accounts
 
-        // Change the variable to this and use Overview as a backup screen if this returns null
-        val currentScreen = rallyTabRowScreens.find { it.route == currentDestination?.route } ?: Overview.Accounts
         Scaffold(
             topBar = {
                 RallyTabRow(
                     allScreens = rallyTabRowScreens,
                     onTabSelected = { newScreen ->
-                        navController
-                            .navigateSingleTopTo(newScreen.route)
+                        navController.navigateSingleTopTo(newScreen.route)
                     },
-                    currentScreen = currentScreen,
+                    currentScreen = currentScreen
                 )
             }
         ) { innerPadding ->
             NavHost(
                 navController = navController,
-                startDestination = Overview.route,
                 modifier = Modifier.padding(innerPadding)
-            ) {
-                composable(route = Overview.SingleAccount.route) {
-                    SingleAccountScreen()
-                }
-            }
-                composable(route = Overview.Accounts.route) {
-                    AccountsScreen()
-                }
-                composable(route = Overview.Bills.route) {
-                    BillsScreen()
-                }
-            }
-            }
+            )
         }
-
-
-fun composable(route: String, function: @Composable () -> Unit) {
-    TODO("Not yet implemented")
-}
-
-
-@Composable
-fun NavHost(navController: NavHostController, startDestination: String, modifier: Modifier, content: () -> Unit) {
-
-}
-
-fun NavHostController.navigateSingleTopTo(route: String) =
-    this.navigate(route) {
-        popUpTo(
-            this@navigateSingleTopTo.graph.findStartDestination().id
-        ) {
-            saveState = true
-        }
-        launchSingleTop = true
-        restoreState = true
     }
+}
--- app/src/main/java/com/example/compose/rally/RallyDestinations.kt
+++ app/src/main/java/com/example/compose/rally/RallyDestinations.kt
@@ -21,17 +21,16 @@ import androidx.compose.material.icons.filled.AttachMoney
 import androidx.compose.material.icons.filled.Money
 import androidx.compose.material.icons.filled.MoneyOff
 import androidx.compose.material.icons.filled.PieChart
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
-import com.example.compose.rally.ui.accounts.AccountsScreen
-import com.example.compose.rally.ui.accounts.SingleAccountScreen
-import com.example.compose.rally.ui.bills.BillsScreen
-import com.example.compose.rally.ui.overview.OverviewScreen
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 
 /**
  * Contract for information needed on every Rally navigation destination
  */
-interface RallyDestination {
+
+sealed interface RallyDestination {
     val icon: ImageVector
     val route: String
 }
@@ -39,26 +38,34 @@ interface RallyDestination {
 /**
  * Rally app navigation destinations
  */
-object Overview : RallyDestination {
+data object Overview : RallyDestination {
     override val icon = Icons.Filled.PieChart
     override val route = "overview"
+}
 
-object Accounts : RallyDestination {
+data object Accounts : RallyDestination {
     override val icon = Icons.Filled.AttachMoney
     override val route = "accounts"
 }
 
-object Bills : RallyDestination {
+data object Bills : RallyDestination {
     override val icon = Icons.Filled.MoneyOff
     override val route = "bills"
 }
 
-object SingleAccount : RallyDestination {
+data object SingleAccount : RallyDestination {
     // Added for simplicity, this icon will not in fact be used, as SingleAccount isn't
     // part of the RallyTabRow selection
     override val icon = Icons.Filled.Money
     override val route = "single_account"
     const val accountTypeArg = "account_type"
+    val routeWithArgs = "$route/{$accountTypeArg}"
+    val arguments = listOf(
+        navArgument(accountTypeArg) { type = NavType.StringType }
+    )
+    val deepLinks = listOf(
+        navDeepLink { uriPattern = "rally://$route/{$accountTypeArg}" }
+    )
 }
 
 // Screens to be displayed in the top RallyTabRow
